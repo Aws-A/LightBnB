548047949db92ac2abee87584187682bc4981adf
Aws-star <awsalrahim@yahoo.com>
2023-04-26 00:30:28 -0400

    Add Property

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -64,6 +64,8 @@ const addUser = function (user) {
     });
 };
 
+
+
 /// Reservations
 
 /**
@@ -141,49 +143,6 @@ const getAllProperties = (options, limit = 10) => {
               })
 
 
-    /*SELECT avg(end_date - start_date) as average_duration
-    FROM reservations;
-
-SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
-FROM properties
-LEFT JOIN property_reviews ON properties.id = property_id
-WHERE city LIKE '%ancouv%'
-GROUP BY properties.id
-HAVING avg(property_reviews.rating) >= 4
-ORDER BY cost_per_night
-LIMIT 10;
-
-SELECT properties.city, count(reservations) as total_reservations
-FROM reservations
-JOIN properties ON property_id = properties.id
-GROUP BY properties.city
-ORDER BY total_reservations DESC;
-
-SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
-FROM reservations
-JOIN properties ON reservations.property_id = properties.id
-JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE reservations.guest_id = 1
-GROUP BY properties.id, reservations.id
-ORDER BY reservations.start_date
-LIMIT 10;
-
-SELECT properties.city, count(reservations) as total_reservations
-FROM reservations
-JOIN properties ON property_id = properties.id
-GROUP BY properties.city
-ORDER BY total_reservations DESC;
-
-SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
-FROM reservations
-JOIN properties ON reservations.property_id = properties.id
-JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE reservations.guest_id = 1
-GROUP BY properties.id, reservations.id
-ORDER BY reservations.start_date
-LIMIT 10;
-
-    */
 };
 
 /**
@@ -192,10 +151,15 @@ LIMIT 10;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(`INSERT INTO properties (title, decription, cost_per_night, parking_spaces, number_of_bathrooms , number_of_bedrooms, country, province, city, street, post_code, active)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $9, $10, $11, $12) returning *`, [title, decription, cost_per_night, parking_spaces, number_of_bathrooms , number_of_bedrooms, country, province, city, street, post_code, active])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
